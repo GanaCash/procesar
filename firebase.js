2a0906f51446c6c494ad7e6b638607ed559ac450
GanaCash <ganacash.oficial@hotmail.com>
2025-04-07 00:28:20 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA4qsn0YKtcpf1TuigTD2ywYIEe9zuD5C0",
+  apiKey: "AIzaSyA4qsn0YKtcpf1TuigTD2ywYIEe9zuD5C0",
   authDomain: "invitaciones-7d704.firebaseapp.com",
   databaseURL: "https://invitaciones-7d704-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "invitaciones-7d704",
-  storageBucket: "invitaciones-7d704.firebasestorage.app",
+  storageBucket: "invitaciones-7d704.appspot.com",
   messagingSenderId: "125520646116",
   appId: "1:125520646116:web:899a20647fe6c8a701c1f1",
   measurementId: "G-KSXGH56RBX"
